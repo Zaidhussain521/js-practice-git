615e390aa55744817a849d63eeae55c0cb980191
Zaidhussain521 <www.zaidhussain8993@gmail.com>
2025-06-18 20:20:12 +0000

    container config done variables, dataType

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1212121
+let accountEmail = "www.zaid@gmail.com"
+var accountPassword = "123456"
+accountcity = "karachi"
+let accountState; 
+
+
+// const accountId = 2 not allowed 
+
+// var not use in javascript
+ 
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"  // yh js ky saary new version chlata hay //
+
+
+//alert(3 + 3)   // hum js ko node.js  mie use kr rahy hien browser mien nhi jbh error ayega
+let name ="zaid"
+let age = 18
+let isLogedIn = true
+let state;
+console.log(3 + 3) 
+
+// premitif dataType
+
+//  string : "". jese name email wagera 
+// number : age .2 to 53 aur is bh aghy 
+//  boolean : true yaah false  krty is mien 
+// null : standalone value  jis ki koi value nhi hay 
+// undefined : jw ky abh  defined nhi hui hay 
+// symbol : uniqe
+
+// object : is ky bht episode mien baat hwgi hay 
+
+
+console.log(typeof null);  //Object
+
+
+console.log(typeof undefined); //undefined
+
+
