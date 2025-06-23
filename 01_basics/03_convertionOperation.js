21d031425d50a583b0c1ffe1385fe17ef01f999e
Zaidhussain521 <www.zaidhussain8993@gmail.com>
2025-06-23 10:15:12 +0000

    convertion of dataType

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,44 @@
+let score = null
+
+
+console.log(typeof score);
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" yh string hay  => yh number mien convert hwraha hay 
+// "33abc" yh bh aik string hay => NaN dyrha hay jis ka mtlb not a number hwta hay aur yh aik number hay 
+// true =>  tw 1 dya jwky theek hay 
+// false => yh 0 dya hay 
+
+
+let IsLoggedIn = "zaid"
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+// 1 => true ; fasle => 0
+// "" => fasle ; "zaid" => true 
+
+
+let someNumber = 33 
+
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
