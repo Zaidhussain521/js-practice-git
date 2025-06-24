2727a002040023ea46186156932dc493e8cc7498
Zaidhussain521 <www.zaidhussain8993@gmail.com>
2025-06-24 12:22:39 +0000

    covertion and camparison dataType

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -1,13 +1,13 @@
 let score = null
 
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" yh string hay  => yh number mien convert hwraha hay 
 // "33abc" yh bh aik string hay => NaN dyrha hay jis ka mtlb not a number hwta hay aur yh aik number hay 
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 let IsLoggedIn = "zaid"
 
 let BooleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 // 1 => true ; fasle => 0
 // "" => fasle ; "zaid" => true 
@@ -29,9 +29,61 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+//   ***************************** OPeration *********************************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+
+
+let str1 = "areeb"
+let str2 = " zaid"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log(3 + 2 * 2 % 2); aese likhna not allowed hay //
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
+
+
+ let gameCounter = 100
+ ++gameCounter;
+  
+ console.log(gameCounter);
+
+ 
+ 
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,37 @@
+// console.log(2 > 1)
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1); // yh use nh krna chaiye maghr kbh aesa ajata hay kaam clean code likhna behtr
+
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+//  yh shi ni hay code hamesha clean hona chaiye //
+
+// ===
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
