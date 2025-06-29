27d265fb46589bc5e9e0d900a31452248e961fbd
Zaidhussain521 <www.zaidhussain8993@gmail.com>
2025-06-29 12:58:27 +0000

    Datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,41 @@
+// primitive datatype
+ 
+// 7 type string, Number, boolean, null, undfined, bigInt, symbol
+
+
+
+// const score = 100 
+// const scoreValue = 100.0
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 383873773738837382783n
+
+
+
+//Refrence or non primitive datatype
+// array, object, funtion 
+
+const heros = ["zaid", "kashmair", "baloshistan"]
+
+let myObj ={
+   name : "zaid",
+    age : 18,
+    
+
+}
+
+const myFunction = function(){
+    console.log("hello world")
+    
+    
+}
+
+
